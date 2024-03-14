7268b80ceaab5259ba52ec9d2b72287e687ad931
s25510 <s25510@pjwstk.edu.pl>
2024-03-14 15:12:32 +0100

    MAX number

---
+++ MAX/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine(GetMax([1,2,3,4,5]));
+
+static double GetMax(int[] arr)
+{
+    int max = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (max < arr[i])
+        {
+            max = arr[i];
+        }
+    }
+
+    return max;
+}
