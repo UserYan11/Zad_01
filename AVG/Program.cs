0910fdd595a605f03cc3968a09087303af1e1bea
s25510 <s25510@pjwstk.edu.pl>
2024-03-14 14:45:33 +0100

    New commit

---
+++ AVG/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine(GetAvg([1,2,3,4,5]));
+
+static double GetAvg(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        sum += arr[i];
+    }
+
+    return sum / arr.Length;
+}
