dfeb4756a1cc192c53e95ddc7cb4fa0ee8775f68
UserYan11 <95740495+UserYan11@users.noreply.github.com>
2024-03-14 14:54:42 +0100

    Delete MyApp directory

--- MyApp/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("How are you?");
-Console.WriteLine("This is 2th modification");
-Console.WriteLine("No, it's a 3th modification");
